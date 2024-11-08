4b841169e89c1be9127c5fad9e4e9ed24b8654a2
Md Zahidul Islam Sumon <sumon.bem@gmail.com>
2024-11-08 17:02:02 +0600

    inserting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
@@ -28,9 +32,14 @@ public class DemoApplication {
     @Bean
     CommandLineRunner commandLineRunner() {
         return args -> {
-            var res = sourceJdbcTemplate.queryForList("select * from bem_music.music", Map.of());
-            System.out.println(res);
-            // TODO: make transformation and insert to target db
+            List<Map<String, Object>> rows = sourceJdbcTemplate.queryForList("select * from bem_music.music", Map.of());
+            for (Map<String, Object> row : rows) {
+                String sql = "INSERT INTO music (id) VALUES (:id)";
+                MapSqlParameterSource parameters = new MapSqlParameterSource();
+                parameters.addValue("id", row.get("oid"));
+                targetJdbcTemplate.update(sql, parameters);
+                log.info("inserted: {}", row.get("oid"));
+            }
         };
     }
 }
